d625b7de3d152914f1c98014aafc500b63858151
Victoria Jeon <victoriahjeon@gmail.com>
2021-06-15 12:22:49 -0400

    product model completed

---
+++ server/db/models/product.js
@@ -0,0 +1,38 @@
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+const Product = db.define('product', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+        }
+    },
+    price: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+        },
+    },
+    description: {
+        type: Sequelize.TEXT,
+    },
+    quantity: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+        validate: {
+            min: 0,
+        }
+    },
+    imageUrl: {
+        type: Sequelize.TEXT,
+        validate: {
+            isUrl: true,
+        },
+        defaultValue: 'https://media.giphy.com/media/yZRoXvpZflasovKcmN/giphy.gif',
+    },
+});
+
+module.exports = Product;
