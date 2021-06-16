f970873df3fceb70110ec7cff5fad0658270c2ae
Phoebe Torchia <phoebetorchia@Phoebes-Air.fios-router.home>
2021-06-16 11:34:39 -0400

    updated order status field

--- server/db/index.js
+++ server/db/index.js
@@ -8,21 +8,13 @@ const Product = require('./models/product');
 
 const Order_Product = require('./models/order_product');
 
-const Cart = require('./models/cart');
-
 const Order = require('./models/order');
-
-const Cart_Product = require('./models/cart_product');
 
 //associations could go here!
 User.hasMany(Order);
 Order.belongsTo(User);
 Order.belongsToMany(Product, { through: Order_Product });
 Product.belongsToMany(Order, { through: Order_Product });
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Product.belongsToMany(Cart, { through: Cart_Product })
-Cart.belongsToMany(Product, { through: Cart_Product })
 
 module.exports = {
   db,
@@ -30,8 +22,6 @@ module.exports = {
     User,
     Product,
     Order_Product,
-    Cart,
     Order,
-    Cart_Product,
   },
 };
