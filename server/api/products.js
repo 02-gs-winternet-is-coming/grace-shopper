f6e530bab28001448db31aa8e55c52414d30aa8b
Ella Heron <heronsound95@gmail.com>
2021-06-17 17:38:37 -0400

    finishes singleProduct api routes

--- server/api/products.js
+++ server/api/products.js
@@ -1,6 +1,16 @@
 const router = require('express').Router();
+const {models: User, Product} = require('../db/');
 
-const Product = require('../db/models/product');
+async function requireToken(req, res, next) {
+  try {
+    const token = req.headers.authorization;
+    const userObj = await User.findByToken(token);
+    req.user = userObj;
+    next();
+  } catch (error) {
+    next(error);
+  }
+}
 
 router.get('/', async (req, res, next) => {
   try {
@@ -11,19 +21,50 @@ router.get('/', async (req, res, next) => {
   }
 })
 
-//GET api/products/:id
+// Admin only
+router.post('/', requireToken, async (req, res, next) => {
+  if (req.user.isAdmin) {
+    try {
+      const product = await Product.create(req.body);
+      res.status(201).send(product);
+    } catch (err) {next(err)}
+  }
+})
+
 router.get('/:productId', async (req, res, next) => {
     try {
         let product = await Product.findOne({
-            where: {
-                id: req.params.id,
-            },
+            where: { id: req.params.productId }
         });
         res.json(product);
     } catch (error) {
         next(error);
-    };
+    }
 });
+
+// Admin only
+router.put('/:productId', requireToken, async (req, res, next) => {
+  if (req.user.isAdmin) {
+    try {
+      let product = await Product.findOne({
+        where: { id: req.params.productId }
+      });
+      await product.update(req.body);
+      res.status(201).send(product);
+    } catch (err) {next(err)}
+  }
+})
+
+// Admin only
+router.delete('/:productId', async (req, res, next) => {
+  if (req.user.isAdmin) {
+    try {
+      const product = await Product.findByPk(req.params.productId);
+      await product.destroy();
+      res.status(201).send('successful deletion');
+    } catch (err) {next(err)}
+}
+})
 
 module.exports = router;
 
