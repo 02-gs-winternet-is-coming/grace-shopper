2e20d8ac93cb8771d404dc3f49af04e33c622c35
Victoria Jeon <victoriahjeon@gmail.com>
2021-06-17 13:00:35 -0400

    order post route

--- server/api/orders.js
+++ server/api/orders.js
@@ -7,17 +7,26 @@ const {
 //route: '/orders/:userId
 router.post('/:userId', async (req, res, next) => {
     try {
+        //find open order based on userId
         const currentOrder = await Order.findOne({
             where: {
                 userId: req.params.userId, status: 'open'
             }
         })
-        const product = await Product.findByPk(req.body.id)
+        //if user does not have an open order, create one for the user
         const user = await User.findByPk(req.params.userId)
         if (!currentOrder) {
             currentOrder = await Order.create()
-            user.
+            user.addOrders(currentOrder)
         }
-
+        //add the product from incoming body to the current order of the user
+        const product = await Product.findByPk(req.body.id)
+        await currentOrder.addProducts(product)
+        //send status code and the product you want to add to the cart
+        res.status(201).send(product)
+    } catch (err) {
+        next(err)
     }
 })
+
+module.exports = router
