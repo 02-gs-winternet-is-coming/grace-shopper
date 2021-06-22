a238fdfc097978b2284c4cc0aec6c19f9e258468
Ella Heron <heronsound95@gmail.com>
2021-06-22 15:19:59 -0400

    adds link to checkout component

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,13 +1,14 @@
 import axios from 'axios'
 import React from 'react'
 import { connect } from 'react-redux'
-import { fetchCart, deleteProductThunk, addToCartThunk, deleteQuantityThunk } from '../store/cart'
+import { fetchCart, deleteProductThunk, addToCartThunk } from '../store/cart'
+import { fetchSingleProduct } from '../store/singleProduct'
+import { Link } from 'react-router-dom'
 
 class Cart extends React.Component {
     constructor() {
         super()
         this.incrementQuantity = this.incrementQuantity.bind(this)
-        this.decrementQuantity = this.decrementQuantity.bind(this)
     }
     async componentDidMount() {
         let id = Number(this.props.match.params.userId)
@@ -16,55 +17,56 @@ class Cart extends React.Component {
     async componentDidUpdate(prevProps){
         if (prevProps.userId !== this.props.userId) {
             await this.props.getCart(this.props.userId);
-        }  
+        }
     }
     async incrementQuantity(event) {
-        const quantityType = event.target.value
-        const { data } = await axios.get(`/api/products/${event.target.id}`)
-        await this.props.increment([this.props.cart.userId, data, quantityType])
-    }
-    async decrementQuantity(event) {
-        const quantityType = event.target.value
-        const { data } = await axios.get(`/api/products/${event.target.id}`)
-        await this.props.decrement([this.props.cart.userId, data, quantityType])
+        const {data} = await axios.get(`/api/products/${event.target.id}`)
+        await this.props.updateCart([this.props.cart.userId ,data])
     }
 
     render() {
         let cartProducts = this.props.cart.products || []
         let userId = Number(this.props.match.params.userId)
-        const stringTotal = cartProducts.reduce((accum, product) => {
+        const total = cartProducts.reduce((accum, product) => {
             let subTotal = product.orderProduct['quantity'] * product.price
             return accum + subTotal
-        }, 0).toFixed(2)
-        
+        }, 0)
+
         return (
             <div>
-                {this.props.isLoggedIn && cartProducts.length > 0 &&
+                {cartProducts.length > 0 &&
                 cartProducts.map(product => {
                     return (
                         <div key={product.orderProduct['productId']}>
-                            <img src={product.imageUrl} /> 
+                            <img src={product.imageUrl} />
                             <h1>{product.name} <button onClick={() => this.props.deleteProduct(product.orderProduct['productId'], product.name, userId)}>Remove</button> </h1>
                             <p>${product.price}</p>
                             <p>{product.description}</p>
-                            <p>quantity: {product.orderProduct['quantity']} <button id={product.orderProduct['productId']} value={"decrement"} onClick={this.decrementQuantity}>-</button> <button id={product.orderProduct['productId']} value={"increment"} onClick={this.incrementQuantity}>+</button> </p> 
+                            <p>quantity: {product.orderProduct['quantity']} <button>-</button> <button id={product.orderProduct['productId']}onClick={this.incrementQuantity}>+</button> </p>
                         </div>
 
                     )
-                })
-
+                })}
                 {this.props.guestCart.length > 1 && this.props.guestCart.map(product => {
                        return(
                         <div key={product.id}>
-                       <img src={product.imageUrl} /> 
+                       <img src={product.imageUrl} />
                        <h1>{product.name}</h1>
                        <p>quantity: {product.quantity} <button>-</button> <button>+</button></p>
                        <p>{product.price}</p>
-                       </div>)  
+                       </div>)
                 })}
-               <p>total: ${Number(stringTotal)}</p>
-            <div><button> Clear Cart </button> <button>Check Out</button></div>
-
+                <p>total: ${total}</p>
+                <div>
+                    <button>
+                        Clear Cart
+                    </button>
+                    <Link to={`/cart/checkout/${userId}`}>
+                        <button>
+                            Check Out
+                        </button>
+                    </Link>
+                </div>
             </div>
         )
     }
@@ -81,8 +83,8 @@ const mapDispatchToProps = (dispatch, { history }) => {
     return {
         getCart: (id) => dispatch(fetchCart(id)),
         deleteProduct: (productId, productName, userId) => dispatch(deleteProductThunk(productId, productName, userId, history)),
-        increment: (infoObj) => dispatch(addToCartThunk(infoObj, history)),
-        decrement: (infoObj) => dispatch(deleteQuantityThunk(infoObj, history)),
+        updateCart: (infoObj) => dispatch(addToCartThunk(infoObj, history)),
+        fetch: (id) => dispatch(fetchSingleProduct)
     }
 }
 
